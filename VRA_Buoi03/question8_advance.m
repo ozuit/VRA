912230cc1ea2924ac39bf8fb498c238ae459182e
duy tan <duytan.uit@gmail.com>
2017-10-10 23:53:34 +0700

    update thuc hanh buoi 3

--- VRA_Buoi03/question8_advance.m
+++ VRA_Buoi03/question8_advance.m
@@ -1,30 +1,16 @@
-function question8_advance()
+function question8_advance(k)
     imgTrainAll = loadMNISTImages('./train-images.idx3-ubyte');
     lblTrainAll = loadMNISTLabels('./train-labels.idx1-ubyte');
     
-    Mdl = fitcknn(imgTrainAll' , lblTrainAll);
+    Mdl = fitcknn(imgTrainAll' , lblTrainAll, 'NumNeighbors', k);
     
     imgTestAll = loadMNISTImages('./t10k-images.idx3-ubyte');
     lblTestAll = loadMNISTLabels('./t10k-labels.idx1-ubyte');
     
-    arrImagesNTest = [];
-    for i = 1:size(imgTestAll, 2)
-       if (lblTestAll(i) == 0)
-          arrImagesNTest = [arrImagesNTest, imgTestAll(:, i)];
-       end
-    end
-    numImagesNTest = size(arrImagesNTest,2);
-    fprintf('So luong anh test co label 0 la %d\n', size(arrImagesNTest,2));
-    
-    countFailure = 0;
-    for i = 1:numImagesNTest
-        imgTest = arrImagesNTest(:, i);
-        lblPredictTest = predict(Mdl, imgTest');
-        if (lblPredictTest ~= 0)
-            countFailure = countFailure + 1;
-        end
-    end
-    fprintf('So luong anh co label 0 bi nhan dang sai la: %d\n', countFailure);
-    percentageAccuracy = (numImagesNTest - countFailure)*100/numImagesNTest;
-    fprintf('=> Do chinh xac cua thuat toan knn voi 1 nearest neighbors la %d\n', round(percentageAccuracy, 2));
+    lblResult = predict(Mdl,imgTestAll');
+	nResult = (lblResult == lblTestAll);
+	nCount = sum(nResult);
+
+    percentageAccuracy = nCount*100/size(imgTestAll, 2);
+    fprintf('=> Do chinh xac cua thuat toan knn voi %d nearest neighbors la %.2f\n', k, percentageAccuracy);
 end
