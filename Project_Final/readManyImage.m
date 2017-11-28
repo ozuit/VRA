e2f1f1c370f535f7004fccb6a119bece574814b6
duy tan <duytan.uit@gmail.com>
2017-11-28 23:01:55 +0700

    fix bug and save image croped to disk

--- Project_Final/readManyImage.m
+++ Project_Final/readManyImage.m
@@ -4,6 +4,7 @@ function readManyImage()
         files = dir(fullfile(d, '*.jpg'));
         folders = strsplit(d, '/');
         image_label = folders(size(folders,2));
+        mkdir(['CropedImages/' char(image_label)]);
         all_images = [];
         h = waitbar(0,'Đang xử lý...');
         for i=1:size(files,1)
@@ -11,7 +12,13 @@ function readManyImage()
             croped_images = cropFace(image_path);
             if (size(croped_images,2) > 0)
                 for j=1:size(croped_images,2)
-                    all_images = [all_images, croped_images(:,1)];
+                    %% Store image in an array
+                    all_images = [all_images, croped_images(:,j)];
+                    %% Wite image to disk
+                    strFileName = ['image_' num2str(i) '.jpg'];
+                    strPath = ['CropedImages/' char(image_label) '/' strFileName];
+                    img2D = reshape(croped_images(:,j), 112, 92);
+                    imwrite(img2D, strPath);
                 end
             end
             waitbar(i / size(files,1));
